b277a996136db84bddeaa75d454f21e6dea7d70d
thealmostkid <nat@alumni.brown.edu>
2013-12-23 08:58:16 -0600

    Refactored PlayerMover into Repeatable for general purpose.

--- Repeatable.js
+++ Repeatable.js
@@ -1,21 +1,20 @@
-function PlayerMover(updateFunc) {
+function Repeatable(updateFunc, interval) {
 	this.updateFunc = updateFunc;
+	this.interval = interval;
 	this.timer = undefined;
 };
 
-PlayerMover.prototype.updatePosition = function() {
-};
-
-PlayerMover.prototype.startMoving = function() {
+Repeatable.prototype.start = function() {
 	var update = this.updateFunc;
+	var period = this.interval;
 	if (this.timer === undefined) {
 		this.timer = setInterval(function() {
 			update();
-		}, MOVE_INTERVAL_MS);
+		}, period);
 	}
 };
 
-PlayerMover.prototype.stopMoving = function() {
+Repeatable.prototype.stop = function() {
 	if (this.timer !== undefined) {
 		clearInterval(this.timer);
 		this.timer = undefined;
