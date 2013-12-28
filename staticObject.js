7568a61746ce7ad278dc4f0aaf67dfe041403291
thealmostkid <nat@alumni.brown.edu>
2013-12-28 14:52:18 -0600

    Create StaticObject enum.

---
+++ staticObject.js
@@ -0,0 +1,16 @@
+function StaticObject(type) {
+  this.type = type;
+}
+
+StaticObject.EMPTY = 0;
+StaticObject.FLOOR = 1;
+StaticObject.NORTH_WALL = 2;
+StaticObject.EAST_WALL = 3;
+StaticObject.SOUTH_WALL = 4;
+StaticObject.WEST_WALL = 5;
+StaticObject.CORNER = 6;
+StaticObject.DOOR = 7;
+
+if (module !== undefined) {
+  module.exports.StaticObject = StaticObject;
+}
