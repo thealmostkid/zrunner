9808639c022bbaef5a2e30704276b0bb441097d9
thealmostkid <nat@alumni.brown.edu>
2013-12-29 22:21:01 -0600

    Add player html renderer.

---
+++ test/playerHTMLRendererTest.js
@@ -0,0 +1,14 @@
+var should = require('should');
+var PlayerHTMLRenderer = require(__dirname + '/../playerHTMLRenderer.js').PlayerHTMLRenderer;
+
+describe('PlayerHTMLRenderer', function(){
+  var renderer = undefined;
+  beforeEach(function(){
+    renderer = new PlayerHTMLRenderer();
+  });
+
+  describe('#update()', function(){
+    it('should change dom element', function(){
+    });
+  });
+});
